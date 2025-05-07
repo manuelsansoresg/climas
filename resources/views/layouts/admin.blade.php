2a7576033ba3a18e5ef3e05083e6f9e70be0085d
manuelsansoresg <manuelsansoresg@gmail.com>
2025-05-06 18:28:50 -0600

    Update meta description in admin layout to reflect new sales management system

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="author" content="Softnio">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
+    <meta name="description" content="Sistema de gestión de ventas y reportes para Backti">
     <!-- Fav Icon  -->
     <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}">
     <!-- Page Title  -->
