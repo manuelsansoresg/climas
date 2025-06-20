b9fde7e632c9c425eab1a22fcb61090d48d289b0
manuelsansoresg <manuelsansoresg@gmail.com>
2025-06-20 13:16:05 -0600

    Update author information in layouts: Change author meta tag in auth layout and footer copyright in admin layout from "Softnio" to "Backti".

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -734,7 +734,7 @@
                 <div class="nk-footer">
                     <div class="container-fluid">
                         <div class="nk-footer-wrap">
-                            <div class="nk-footer-copyright"> &copy; 2024 DashLite. Template by <a href="https://softnio.com" target="_blank">Softnio</a>
+                            <div class="nk-footer-copyright"> &copy; 2024 Backti
                             </div>
                             <div class="nk-footer-links">
                                 <ul class="nav nav-sm">
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="author" content="Softnio">
+    <meta name="author" content="Backti">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description"
         content="Una Sistema de gestión de ventas y reportes para Backti">
